5bd45a4285d4a0135057fbeab32f2523694ce616
Leonid Kukuyev <leonidkukuyev@gmail.com>
2015-04-29 23:00:27 -0400

    Initial Commit

---
+++ src/detector/ScoreDetector.java
@@ -0,0 +1,22 @@
+package detector;
+
+public class ScoreDetector {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public static String parseReviews(String reviews) {
+		//add textfile writing function
+		
+		//add textfile reading function
+		int score = 4;
+		return Integer.toString(score);
+	}
+
+}

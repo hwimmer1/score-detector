c123fba06d2ad05e0f726772733d8912cf7ff8d8
Leonid Kukuyev <leonidkukuyev@gmail.com>
2015-05-05 09:13:35 -0400

    Update ScoreDetector.java

--- src/detector/ScoreDetector.java
+++ src/detector/ScoreDetector.java
@@ -3,6 +3,10 @@ package detector;
 
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ScoreDetector {
 
@@ -13,7 +17,6 @@ public class ScoreDetector {
 	// CONTINUE - Incorporate file reader. Host on Heroku to test if overwriting will work while on Heroku.
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		parseReviews("TESTERa fndsai sdin");
 		
 	}
 	
@@ -25,18 +28,28 @@ public class ScoreDetector {
 		
 		System.out.println(path);
 		
+		//Textfile Writer
 		PrintWriter out = new PrintWriter(path);
 		out.println(reviews);
 		out.close();
 	
+		//Textfile Reader
+		String content = readFile("reviews.txt", StandardCharsets.UTF_8);
+		//THIS IS WHERE ANY COUNTERFEIT DETECTION OPERATIONS TAKE PLACE
+		//SHOULD RETURN SCORE
+		//SCORE SHOULD BE CONVERTED TO STRING AND RETURNED
 		
-		//add textfile writing function
+		//int score = 4;
+		//return Integer.toString(score);
 		
-		//add textfile reading function
-		int score = 4;
-		return Integer.toString(score);
+		//For now, Textfile content is returned to test Heroku writing function
+		return content;
 	}
 	
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
 	
 
 }
